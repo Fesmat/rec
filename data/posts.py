dc64ac996b1fe9beb670fba04279ea4e0aa43bb0
Fesmat <dden4929@gmail.com>
2021-04-04 22:34:48 +0300

    User, post classes added. Db file added. Main edited fo testing.

---
+++ data/posts.py
@@ -0,0 +1,19 @@
+import sqlalchemy
+from sqlalchemy import orm
+from sqlalchemy_serializer import SerializerMixin
+from .db_session import SqlAlchemyBase
+
+
+class Post(SqlAlchemyBase, SerializerMixin):
+    __tablename__ = 'posts'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    creator_id = sqlalchemy.Column(sqlalchemy.Integer,
+                                   sqlalchemy.ForeignKey("users.id"), nullable=False)
+    film_id = sqlalchemy.Column(sqlalchemy.Integer,
+                                sqlalchemy.ForeignKey("films.id"), nullable=False)
+    text = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+
+    user = orm.relation('User')
+    film = orm.relation('Film')
---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from data import db_session
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '1Aj3sL12J09d43Ksp02A'
+
+
+@app.route('/', methods=['GET'])
+@app.route('/index', methods=['POST'])
+def index():
+    return 'Test'
+
+
+if __name__ == '__main__':
+    db_session.global_init("db/blogs.db")
+    app.run(port=8080, host='127.0.0.1')
