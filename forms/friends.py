143b556477cdf297a2f3be24b8d831c18af8b827
Kirill <kdvorobyev@gmail.com>
2021-04-22 23:44:31 +0300

    Добавил страницу друзей.

---
+++ forms/friends.py
@@ -0,0 +1,9 @@
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileRequired
+from wtforms import StringField, SubmitField
+
+
+class FriendsForm(FlaskForm):
+    username = StringField('Кирилл Воробьев')
+    user_photo = FileField(validators=[FileRequired()])
+    unfriend = SubmitField('Удалить из друзей')
